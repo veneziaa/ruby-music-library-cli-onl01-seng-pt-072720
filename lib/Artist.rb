e950d2a6f9361112e34ff3cdda5266f5795956dc
Venice Vargas <venicevargas1@live.com>
2020-09-21 02:30:04 +0000

    Automatically backed up by Learn

---
+++ lib/Artist.rb
@@ -0,0 +1,47 @@
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    # self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+
+      song.artist = self unless song.artist
+      songs << song unless songs.include?(song)
+     
+
+  end
+
+  def genres
+   songs.collect { |song| song.genre}.uniq
+  end
+end
